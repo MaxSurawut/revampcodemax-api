ccc776c8e81254c10c91d6a358edf87e19da239c
MaxSurawut <surawut.bao@gmail.com>
2023-05-15 15:06:33 +0700

    completed

--- admin/adminSchema.js
+++ admin/adminSchema.js
@@ -12,47 +12,47 @@ const createAdminTable = () => {
         password VARCHAR(255) NOT NULL
     )`
 
-    db.connection.query(sql, (err, result)=>{
+    db.connection.query(sql, (err, result) => {
         if (err) {
             console.error('Error creating Admin table: ', err);
             return;
-          }
-  
-          console.log('Admin table created successfully!');
+        }
+
+        console.log('Admin table created successfully!');
     })
 }
 
 const loginValidation = (email, password, res) => {
-    if(!email || !password){
-        return res.status(400).json({message : 'Email and password are required'})
-      }
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Email and password are required' })
+    }
 
     const sql = 'SELECT * FROM admin WHERE email =  ?'
 
     db.connection.query(sql, [email], (err, result) => {
-        if(err){
+        if (err) {
             console.error(err)
-            return res.status(500).json({ message: 'Internal server erorr'})
+            return res.status(500).json({ message: 'Internal server erorr' })
         }
 
-        if(result.length === 0){
-            return res.status(401).json({ message: 'Email or password is incorrect'})
+        if (result.length === 0) {
+            return res.status(401).json({ message: 'Email or password is incorrect' })
         }
 
         const user = result[0];
         bcrypt.compare(password, user.password, (err, isMatch) => {
-            if(err){
+            if (err) {
                 console.error(err)
-                return res.status(500).json({ message: 'Internal server erorr'})
+                return res.status(500).json({ message: 'Internal server erorr' })
             }
 
-            if(!isMatch){
-                return res.status(401).json({ message: 'Email or password is incorrect'})
+            if (!isMatch) {
+                return res.status(401).json({ message: 'Email or password is incorrect' })
             }
 
-            const token = jwt.sign({ id:user.id, email: user.email}, process.env.JWT_SECRET)
+            const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET)
 
-            return res.status(200).json({token})
+            return res.status(200).json({ token })
         })
     })
 }
@@ -60,28 +60,44 @@ const loginValidation = (email, password, res) => {
 const adminRegister = (email, password, res) => {
 
     bcrypt.genSalt(saltRounds, (err, salt) => {
-        bcrypt.hash(password, salt , (err, hash) => {
-            if(err){
+        bcrypt.hash(password, salt, (err, hash) => {
+            if (err) {
                 console.error('Error hashing password:', err)
-                return res.status(500).json({ message: 'Server error'})
+                return res.status(500).json({ message: 'Server error' })
             }
-    
+
             const sql = 'INSERT INTO admin (email, password) VALUES (?, ?)'
             db.connection.query(sql, [email, hash], (err, result) => {
-                if(err){
+                if (err) {
                     console.error('Error inserting admin:', err);
-                    return res.status(500).json({ message: 'Server error'})
+                    return res.status(500).json({ message: 'Server error' })
                 }
-    
-                res.json({message : 'Admin registered successfully'})
+
+                res.json({ message: 'Admin registered successfully' })
             })
         })
     })
-    
+
+}
+
+const validateToken = (token, req, res, next) => {
+    if (!token) {
+        return res.status(401).json({ message: 'Missing token' })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        console.error(err);
+        return res.status(401).json({ message: 'Invalid token' })
+    }
 }
 
 module.exports = {
     createAdminTable,
     loginValidation,
-    adminRegister
+    adminRegister,
+    validateToken
 }
--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const cors = require('cors')
 const app = express();
 
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 app.use(cors({
-  origin: '*'
+  origin: 'http://127.0.0.1:5173'
 }))
 
 
@@ -53,6 +54,13 @@ app.post('/register', (req, res) => {
 
   adminModule.adminRegister(email, password, res)
 })
+
+app.post('/validate-token', (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1];
+  console.log(token)
+  adminModule.validateToken(token,req, res, next)
+})
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
